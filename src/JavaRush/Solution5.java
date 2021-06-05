764438c8eb8b8c149ef782cab79dc81c51ea22bb
MrKirill0325 <78133560+MrKirill0325@users.noreply.github.com>
2021-06-05 10:44:11 +0300

    initial commit

---
+++ src/JavaRush/Solution5.java
@@ -0,0 +1,36 @@
+package JavaRush;
+
+import java.util.Scanner;
+
+public class Solution5 {
+
+    public static Scanner scanner = new Scanner(System.in);
+    public static int[][] multiArray;
+
+    public static void main (String[] args){
+
+        int N=scanner.nextInt();
+
+        multiArray = new int[N][];
+        for (int i=0; i<N;i++){
+            int numberColumn = scanner.nextInt();
+            multiArray[i] = new int [numberColumn];
+        }
+    }
+}
+
+
+/*
+РЕШЕНИЕ JAVARUSH
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        multiArray = new int[n][];
+        for (int i = 0; i < n; i++) {
+            int size = Integer.parseInt(scanner.nextLine());
+            multiArray[i] = new int[size];
+        }
+    }
+ */
